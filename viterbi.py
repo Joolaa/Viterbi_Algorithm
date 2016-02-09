62340ff98edac38cbfb9f156bffd2a2a15176685
Joolaa <jola@cs.helsinki.fi>
2016-02-09 22:21:06 +0200

    added labeling to output and the probabilities

--- viterbi.py
+++ viterbi.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+def convert_to_labels(indices, labels):
+    result = []
+    for index in indices:
+        result.append(labels[index])
+    return result
+
 #Implementation of the Viterbi algorithm.
 #
 #Given a sequence of observations (events)
@@ -9,7 +15,8 @@ import numpy as np
 #sequence of hidden states that could have
 #emitted the events given as the argument.
 def viterbi(events, transit_probs,
-            emission_probs, init_probs):
+            emission_probs, init_probs,
+            event_labels):
     probs_table = np.zeros((len(events), len(transit_probs)))
     nodes_table = np.zeros((len(events), len(transit_probs)),
                            dtype=np.dtype(int))
@@ -27,14 +34,20 @@ def viterbi(events, transit_probs,
             probs_table[i, j] = max_val
             nodes_table[i, j] = max_ix
     result_indices = []
+    result_probs = []
+    result_probs.append(np.amax(probs_table[len(probs_table) - 1]))
     previous_index = np.argmax(probs_table[len(probs_table) - 1])
     result_indices.append(previous_index)
     for i in range(len(probs_table) - 1, 0, -1):
         current_index = nodes_table[i, previous_index]
         result_indices.append(current_index)
+        result_probs.append(probs_table[i, previous_index])
         previous_index = current_index
     result_indices.reverse()
-    return result_indices
+    result_probs.reverse()
+    if event_labels:
+        result_indices = convert_to_labels(result_indices, event_labels)
+    return result_indices, result_probs
 
 
 #test_transits = np.array([[1.0/3.0, 1.0/3.0, 1.0/3.0, 0],
@@ -65,6 +78,14 @@ def viterbi(events, transit_probs,
 #test_inits = np.array([0.2, 0.3, 0.3, 0.2])
 #
 #test_obs = np.array([8, 4, 2, 1, 7, 4])
+#test_labels = ["A", "B", "C", "D"]
 #############################
-#viterbi(test_obs, test_transits, test_emissions, test_inits)
-#=> [2, 2, 3, 1, 0, 1]
+#viterbi(test_obs, test_transits, test_emissions, test_inits, test_labels)
+#=> (['C', 'C', 'D', 'B', 'A', 'B'],
+#    [0.0018,
+#     0.00029399999999999994,
+#     1.1759999999999996e-05,
+#     5.4879999999999962e-07,
+#     2.9269333333333318e-08,
+#     2.9269333333333318e-08])
+
